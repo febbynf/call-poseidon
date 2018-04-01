d9df8b4c9004b97a5fd3c3c9c7423709911e5331
Febby Nurfitriyani <febbynurfitriyani1208@gmail.com>
2018-04-01 20:22:56 +0700

    upload pertama

--- resources/views/grafik_cohen.blade.php
+++ resources/views/grafik_cohen.blade.php
@@ -12,7 +12,7 @@
       <div class="span12">
         <div class="widget-box">
           <div class="widget-title"> <span class="icon"> <i class="icon-signal"></i> </span>
-            <h5>Real Time chart</h5>
+            <h5>Chartsss</h5>
           </div>
           <div class="widget-content">
             <div id="placeholder2"></div>
